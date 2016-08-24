67dec476544cd1d6ad4c6e0be8cf06668519fb64
Xi Cao <caojxi@gmail.com>
2016-08-24 22:58:49 +0930

    add new folders

--- app/Client/Client.php
+++ app/Client/Client.php
@@ -4,12 +4,22 @@ namespace Cmauto\Client;
 
 use Cmauto\Core\EloquentBaseModel;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Cmauto\Car\Car;
 
 class Client extends EloquentBaseModel
 {
     public $timestamps = false;
 
     use SoftDeletes;
+
+    public function cars()
+    {
+      return $this->hasMany(Car::class);
+    }
+
+    public function services()
+    {
+    }
 
     function getSearchableColumns()
     {
